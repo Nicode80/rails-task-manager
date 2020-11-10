9f76f20037c3c3f5306211c556da26060d5a202e
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-11-10 14:20:53 +0100

    create OK

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,14 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.create(task_params)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
